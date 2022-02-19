3dd9127b3f8da4dfb21b52d94f608823e8cc384d
Muhammed <mhmmd.dncr1414@gmail.com>
2022-02-19 16:22:31 +0300

    Add project files.

---
+++ SeleniumSerisi/Bot.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumSerisi
+{
+    public class Bot
+    {
+        IWebDriver driver;
+
+        public void SayfayaGiris()
+        {
+            // Chrome driver yolu 
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService(@"C:/Program Files/Google/Chrome");
+            driver = new ChromeDriver(service); //driver nesnesi türetme
+            driver.Navigate().GoToUrl("https:\\muhammeddincer.com"); // gidilecek sayfa
+        }
+    }
+}
---
+++ SeleniumSerisi/Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumSerisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot b = new Bot();
+            b.SayfayaGiris();
+        }
+    }
+}
